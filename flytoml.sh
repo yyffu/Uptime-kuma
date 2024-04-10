b56d236efe330b8ed1a9f825cac8c3ed651eee26
yyffu <100259439+yyffu@users.noreply.github.com>
2024-04-10 02:02:08 -0800

    Add files via upload

---
+++ flytoml.sh
@@ -0,0 +1,38 @@
+﻿#!/bin/bash
+
+# 随机US节点
+cities=("atl" "bos" "den" "dfw" "ewr" "iad" "mia" "ord")
+index=$(($RANDOM % ${#cities[@]}))
+selected_city=${cities[$index]}
+
+# 输出toml部署文件
+cat > fly.toml << EOF
+# fly.toml app configuration file generated for hgjhghg-damp-pond-5796 on 2024-01-17T04:03:15-08:00
+#
+# See https://fly.io/docs/reference/configuration/ for information about how to use this file.
+#
+
+app = "${FLY_APP_NAME}"
+primary_region = "ewr"
+
+[build]
+  image = "louislam/uptime-kuma:1"
+
+[[mounts]]
+  source = "kuma_data"
+  destination = "/app/data"
+  auto_extend_size_threshold = 0
+
+[http_service]
+  internal_port = 3001
+  force_https = true
+  auto_stop_machines = false
+  auto_start_machines = true
+  min_machines_running = 1
+  processes = ["app"]
+
+[[vm]]
+  cpu_kind = "shared"
+  cpus = 1
+  memory_mb = 256
+EOF
